2af7e57e8a9640e5f817e0a41eecf8bb26bfa324
Arden <aozdere@calpoly.edu>
2019-09-23 21:27:01 -0700

    spaces

--- planets.py
+++ planets.py
@@ -3,7 +3,7 @@ def weight_on_planets():
     earthW = float(earthW)
     marsW = earthW * .38
     jupiterW = earthW * 2.34
-    print("\nOn Mars you would weigh ", marsW, " pounds.", "\nOn Jupiter you would weigh ", jupiterW, " pounds.")
+    print("\nOn Mars you would weigh", marsW, "pounds.\nOn Jupiter you would weigh", jupiterW, "pounds.")
 
 
 if __name__ == '__main__':
